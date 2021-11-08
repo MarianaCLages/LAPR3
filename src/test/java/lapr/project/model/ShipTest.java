4ec98f4be8283bc16753d37e9a9135ef975f9e2e
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-11-08 10:57:34 +0000

    LAPR22G96-32 #time 30m #comment Ship Test Unit

---
+++ src/test/java/lapr/project/model/ShipTest.java
@@ -0,0 +1,266 @@
+package lapr.project.model;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShipTest {
+
+
+    Ship shipgeral = new Ship(111111111, "name", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
+
+    LocalDateTime dateTime =LocalDateTime.now();
+
+    Position posgeral = new Position(dateTime, 0, 0, 1, 0,0);
+
+    @Test
+    public void checkMMSITest() {
+
+        //Arrange
+        //Act
+        try {
+            Ship ship1 = new Ship(111, "name", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
+            Ship ship2 = new Ship(111111111, "name", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
+
+        } catch (IllegalArgumentException ex) {
+            //Assert
+            assertEquals(ex.getMessage(), "MMSI code must have 9 digits!");
+        }
+
+
+    }
+
+    @Test
+    public void checkIMOTest() {
+
+        //Arrange
+        //Act
+        try {
+            Ship ship1 = new Ship(111111111, "name", 11, 1, 1, "A", "A", 1, 1, 1, 1);
+            Ship ship2 = new Ship(111111111, "name", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
+        } catch (IllegalArgumentException ex) {
+            //Assert
+            assertEquals(ex.getMessage(), "IMO code must have 7 digits!");
+        }
+
+
+    }
+
+
+    @Test
+    public void getMmsiTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(111111111, shipgeral.getMmsi());
+    }
+
+    @Test
+    public void setMmsiTest() {
+        //Arrange
+        //Act
+        shipgeral.setMmsi(222222222);
+        //Assert
+        assertEquals(222222222, shipgeral.getMmsi());
+    }
+
+    @Test
+    public void getNameTest() {
+        //Arrange
+        //Act
+        //Assert
+        assertEquals("name", shipgeral.getName());
+
+    }
+
+    @Test
+    public void setNameTest() {
+        //Arrange
+        //Act
+        shipgeral.setName("barco");
+        //Assert
+        assertEquals("barco", shipgeral.getName());
+
+    }
+
+    @Test
+    public void getImoTest() {
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1111111, shipgeral.getImo());
+    }
+
+    @Test
+    public void setImoTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setImo(2222222);
+
+        //Assert
+        assertEquals(2222222, shipgeral.getImo());
+    }
+
+    @Test
+    public void getNumGenTest() {
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1, shipgeral.getNumGen());
+    }
+
+    @Test
+    public void setNumGenTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setNumGen(2);
+        //Assert
+        assertEquals(2, shipgeral.getNumGen());
+
+    }
+
+    @Test
+    public void getCallSignTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals("A", shipgeral.getCallSign());
+    }
+
+    @Test
+    public void setCallSignTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setCallSign("B");
+        //Assert
+        assertEquals("B", shipgeral.getCallSign());
+    }
+
+    @Test
+    public void getVesselTypeTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals("A", shipgeral.getVesselType());
+    }
+
+    @Test
+    public void setVesselTypeTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setVesselType("B");
+        //Assert
+        assertEquals("B", shipgeral.getVesselType());
+    }
+
+    @Test
+    public void getLength() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1, shipgeral.getLength());
+    }
+
+    @Test
+    public void setLengthTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setLength(2);
+        //Assert
+        assertEquals(2, shipgeral.getLength());
+    }
+
+    @Test
+    public void getWidthTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1, shipgeral.getWidth());
+    }
+
+    @Test
+    public void setWidthTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setWidth(2);
+        //Assert
+        assertEquals(2, shipgeral.getWidth());
+    }
+
+    @Test
+    public void getCapacityTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1, shipgeral.getCapacity());
+    }
+
+    @Test
+    public void setCapacityTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setCapacity(2);
+        //Assert
+        assertEquals(2, shipgeral.getCapacity());
+    }
+
+    @Test
+    public void getDraftTest() {
+
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(1, shipgeral.getDraft());
+    }
+
+    @Test
+    public void setDraftTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setDraft(2);
+        //Assert
+        assertEquals(2, shipgeral.getDraft());
+
+    }
+
+    @Test
+    public void setGenPowerOutputTest() {
+
+        //Arrange
+        //Act
+        shipgeral.setGenPowerOutput(2);
+        //Assert
+        assertEquals(2, shipgeral.getGenPowerOutput());
+    }
+
+
+    @Test
+    public void toStringTest() {
+
+        //Arrange
+        String expected = shipgeral.toString();
+        //Act
+        //Assert
+        assertEquals(expected, shipgeral.toString());
+    }
+
+
+}
+
