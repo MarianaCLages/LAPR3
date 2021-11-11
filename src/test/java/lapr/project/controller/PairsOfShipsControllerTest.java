39ddd5377a7dfbb75df0f29a85e467d11de210b6
MiguelJordao <1201487@isep.ipp.pt>
2021-11-11 20:03:47 +0000

    LAPR22G96-83 #comment More Tests (controllers) #time 10m

--- src/test/java/lapr/project/controller/PairsOfShipsControllerTest.java
+++ src/test/java/lapr/project/controller/PairsOfShipsControllerTest.java
@@ -1,12 +1,18 @@
 package lapr.project.controller;
 
+import lapr.project.shared.PairOfShips;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PairsOfShipsControllerTest {
 
+    PairsOfShipsController pairsOfShipsController = new PairsOfShipsController();
+
     @Test
     void getPairs() {
+
+
+
     }
 }
