7ae4a57cd2124bffd4ac13b4d83c778a4bc1c65a
1191008 Rodrigo Rodrigues <1191008@isep.ipp.pt>
2021-11-14 16:56:37 +0000

    LAPR22G96-43 #comment UI redone

--- src/main/java/lapr/project/ui/SearchShipUI.java
+++ src/main/java/lapr/project/ui/SearchShipUI.java
@@ -11,35 +11,89 @@ public class SearchShipUI implements Runnable {
 
     private final SearchShipController ctrl = new SearchShipController();
 
+    public SearchShipUI(){
+
+    }
+
     @Override
     public void run() {
 
         Scanner in = new Scanner(System.in);
         Ship result = null;
-        int op = 0;
-        String para;
+        int op;
+        String parameter;
 
-        System.out.print(
-                "Select which parameter you wish to search a ship by: \n" +
-                        "1 - MMSI\n" + "2 - IMO\n" + "3 - Call Sign\n"+ "\n" + "0 - cancel" );
-        op = Utils.readIntegerFromConsole(in.nextLine().trim());
+        do {
+            try {
+                System.out.print(
+                        "Select which parameter you wish to search a ship by: \n" +
+                                "1 - MMSI\n" + "2 - IMO\n" + "3 - Call Sign\n"+ "\n" + "0 - cancel" );
+                op = Utils.readIntegerFromConsole(in.nextLine().trim());
+                if (op > 3) throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Please enter a valid option!");
+                op = 0;
+            }
+        } while (op == 0);
 
         switch (op){
             case 1:
-                System.out.print("MMSI: \n");
-                op = in.nextInt();
-                result = ctrl.searchShipByMMSI(op);
-                break;
+                do {
+                    try {
+                        op = Utils.readIntegerFromConsole("Enter MMSI: \n");
+                    } catch (NumberFormatException err) {
+                        System.out.println("Please enter a valid MMSI!");
+                        op = 0;
+                    }
+                } while (op == 0);
+                try {
+                    result = ctrl.searchShipByMMSI(op);
+                    break;
+                } catch (IllegalArgumentException err1) {
+                    System.out.println(err1.getMessage());
+                    break;
+                } catch (NullPointerException err2){
+                    System.out.println("Failed to find any ships with those parameters!");
+                    break;
+                }
             case 2:
-                System.out.print("IMO: \n");
-                para = in.nextLine();
-                result = ctrl.searchShipByIMO(para);
-                break;
+                do {
+                    try {
+                        parameter = Utils.readLineFromConsole("Enter IMO: \n");
+                    } catch (NumberFormatException err) {
+                        System.out.println("Please enter a valid IMO!");
+                        parameter = null;
+                    }
+                } while (parameter == null);
+                try {
+                    result = ctrl.searchShipByIMO(parameter);
+                    break;
+                } catch (IllegalArgumentException err1) {
+                    System.out.println(err1.getMessage());
+                    break;
+                } catch (NullPointerException err2){
+                    System.out.println("Failed to find any ships with those parameters!");
+                    break;
+                }
             case 3:
-                System.out.print("Call Sign: \n");
-                para = in.nextLine();
-                result = ctrl.searchShipByCallSign(para);
-                break;
+                do {
+                    try {
+                        parameter = Utils.readLineFromConsole("Enter Call Sign: \n");
+                    } catch (NumberFormatException err) {
+                        System.out.println("Please enter a valid Call Sign!");
+                        parameter = null;
+                    }
+                } while (parameter == null);
+                try {
+                    result = ctrl.searchShipByCallSign(parameter);
+                    break;
+                } catch (IllegalArgumentException err1) {
+                    System.out.println(err1.getMessage());
+                    break;
+                } catch (NullPointerException err2){
+                    System.out.println("Failed to find any ships with those parameters!");
+                    break;
+                }
             default:
                 System.out.println("Invalid input option!");
 
