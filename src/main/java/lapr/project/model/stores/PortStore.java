955af93a8c178e664ffc7dc81dc131aaf746a4a9
MiguelJordao <1201487@isep.ipp.pt>
2021-11-30 19:35:19 +0000

    LAPR22G96-185 #comment Scripts Implementation #time 2h

--- src/main/java/lapr/project/model/stores/PortStore.java
+++ src/main/java/lapr/project/model/stores/PortStore.java
@@ -39,39 +39,131 @@ public class PortStore implements Persistable {
         Connection connection = databaseConnection.getConnection();
         Port port = (Port) object;
 
-        saveContinent(databaseConnection, port);
+        // saveContinent(databaseConnection, port);
+        // saveCountry(databaseConnection,port);
 
-        String sqlCommand = "select * from facility where shipmmsi = ";
+        String sqlCommand = "select * from facility where facilityType = " + port.getClass().getName();
 
         try (PreparedStatement getContainerPreparedStatement = connection.prepareStatement(sqlCommand)) {
-            try (ResultSet addressesResultSet = getContainerPreparedStatement.executeQuery()) {
+            try (ResultSet portsResultSet = getContainerPreparedStatement.executeQuery()) {
 
-                return true;
+                if (portsResultSet.next()) {
 
+                    sqlCommand = "UPDATE FACILITY SET LONGITUDE = " + port.getLocation().getLongitude() + ", LATITUDE = " + port.getLocation().getLongitude() + " where MMSI = " + port.getName();
+
+                    try (PreparedStatement updatePortPreparedStatement = connection.prepareStatement(sqlCommand)) {
+                        updatePortPreparedStatement.executeUpdate();
+                        return true;
+                    }
+
+                } /*else {
+
+                    int facilityType = 1; //Port
+
+                    sqlCommand = "select MAX(FACILITYID) as facilityid from facility";
+
+                    PreparedStatement saveContainerPreparedStatement = connection.prepareStatement(sqlCommand);
+                    resultset = saveContainerPreparedStatement.executeQuery();
+
+                    resultset.next();
+
+                    int idTransportation = resultset.getInt(1) + 1;
+
+                    sqlCommand = "insert into vehicle (vehicleid) values (" + idTransportation + ")";
+
+                    sqlCommand =""
+
+                    return true;
+
+                }
+
+            }/*/
+
+            } catch (SQLException throwables) {
+                System.out.println("PORT NAO DA");
+                return false;
             }
-
         } catch (SQLException throwables) {
-            System.out.println("PORT NAO DA");
-            return false;
+            throwables.printStackTrace();
         }
+
+        return false;
+
     }
 
-    private boolean saveContinent(DatabaseConnection databaseConnection, Port port) {
+    @Override
+    public boolean delete(DatabaseConnection databaseConnection, Object object) {
+        return false;
+    }
+}
+
+   /* private boolean saveContinent(DatabaseConnection databaseConnection, Port port) {
 
         Connection connection = databaseConnection.getConnection();
 
         String sqlCommand = "select * from CONTINENT where NAME = " + port.getContinent();
 
-        try (PreparedStatement getContainerPreparedStatement = connection.prepareStatement(sqlCommand)) {
-            try (ResultSet addressesResultSet = getContainerPreparedStatement.executeQuery()) {
+        try (PreparedStatement getContinentPreparedStatement = connection.prepareStatement(sqlCommand)) {
+            try (ResultSet resultSet = getContinentPreparedStatement.executeQuery()) {
 
-                if (addressesResultSet.next()) {
+                if (resultSet.next()) {
 
+                    //Verificar se é preciso fazer o Update
 
                     return true;
 
                 } else {
 
+                    sqlCommand = "select MAX(COTINENTID) as continentid from continent";
+
+                    PreparedStatement saveContainerPreparedStatement = connection.prepareStatement(sqlCommand);
+                    ResultSet resultset = saveContainerPreparedStatement.executeQuery();
+
+                    resultset.next();
+
+                    int idContinent = resultset.getInt(2) + 1;
+
+                    sqlCommand = "INSERT INTO CONTINENT (CONTINENTID, NAME) VALUES (" + idContinent + ",'" + port.getContinent() + "')";
+
+                    try (PreparedStatement saveContinentPreparedStatement1 = connection.prepareStatement(sqlCommand)) {
+                        saveContinentPreparedStatement1.executeUpdate();
+                    }
+
+                    return true;
+
+                }
+
+            } catch (SQLException e) {
+                System.out.println("NO");
+                return false;
+            }
+
+
+        } catch (SQLException throwables) {
+            System.out.println("CONTINENT NAO DA");
+            return false;
+        }
+
+    } */
+
+   /* private boolean saveCountry(DatabaseConnection databaseConnection, Port port) {
+
+        Connection connection = databaseConnection.getConnection();
+
+        String sqlCommand = "select * from COUNTRY where NAME = " + port.getCountry();
+
+        try (PreparedStatement getCountryPreparedStatement = connection.prepareStatement(sqlCommand)) {
+            try (ResultSet resultSet = getCountryPreparedStatement.executeQuery()) {
+
+                if (resultSet.next()) {
+
+                    //Verificar se é preciso fazer o Update
+
+                    sqlCommand = "";
+
+                    return true;
+
+                } else {
 
                     return true;
 
@@ -95,6 +187,6 @@ public class PortStore implements Persistable {
     public boolean delete(DatabaseConnection databaseConnection, Object object) {
         return false;
     }
-}
+}*/
 
 
