c9fbd3c04376b17cc93c3a07fa06fc25e4e73494
1200902 Mariana Lages <1200902@isep.ipp.pt>
2021-12-02 17:32:29 +0000

    LAPR22G96-150 #comment US207 Script implemented #time 1h 30m

--- src/main/java/lapr/project/controller/App.java
+++ src/main/java/lapr/project/controller/App.java
@@ -2,6 +2,7 @@ package lapr.project.controller;
 
 import lapr.project.data.ConnectionFactory;
 import lapr.project.data.DatabaseConnection;
+import lapr.project.data.DataBaseScripts.AverageCargoByYearScript;
 import lapr.project.model.*;
 import lapr.project.shared.Constants;
 import lapr.project.utils.auth.AuthFacade;
@@ -11,7 +12,6 @@ import lapr.project.utils.auth.domain.OrgRole;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -40,11 +40,11 @@ public class App {
 
         bootstrap();
 
-       // company.getShipStore().delete(databaseConnection,new Ship(999999999,"baseShip","IMO1112222","DDbbCDD","10",10,10,10,"10",'A')); Funciona
+        // company.getShipStore().delete(databaseConnection,new Ship(999999999,"baseShip","IMO1112222","DDbbCDD","10",10,10,10,"10",'A')); Funciona
 
-       // System.out.println(company.getShipStore().getElement(databaseConnection,366934280)); Funciona
+        // System.out.println(company.getShipStore().getElement(databaseConnection,366934280)); Funciona
 
-      //  System.out.println(company.getShipStore().getShipPosition(databaseConnection,"2020-12-31 18:34:00",211331640)); Funfa tbm
+        //  System.out.println(company.getShipStore().getShipPosition(databaseConnection,"2020-12-31 18:34:00",211331640)); Funfa tbm
 
     }
 
@@ -141,19 +141,19 @@ public class App {
         company.getPortStore().add(port2);
 
         //CargoManifest
-        CargoManifest cargoManifest1 = new CargoManifest("11", port1,new Date(2020,11,21));
+        CargoManifest cargoManifest1 = new CargoManifest("11", port1, new Date(2020, 11, 21));
         company.getCargoManifestStore().add(cargoManifest1);
 
-        CargoManifest cargoManifest2 = new CargoManifest("69", port2,new Date(2020,11,21));
+        CargoManifest cargoManifest2 = new CargoManifest("69", port2, new Date(2020, 11, 21));
         company.getCargoManifestStore().add(cargoManifest2);
 
         //Ship
-        Ship ship = new Ship(222222222,"aa","IMO1111111",11,11,"AA","70",30,30,30,30);
+        Ship ship = new Ship(222222222, "aa", "IMO1111111", 11, 11, "AA", "70", 30, 30, 30, 30);
         ship.getCargoManifestAVL().insert(cargoManifest1);
         ship.getCargoManifestAVL().insert(cargoManifest2);
 
 
-       // company.getShipStore().save(databaseConnection,ship);
+        // company.getShipStore().save(databaseConnection,ship);
 
         //Position
         String sdate = "31/11/2020 23:16";
@@ -166,6 +166,10 @@ public class App {
         ship.addLoadedContainer(container1, port1);
         ship.addOffLoadedContainer(container1, port1);
         company.getShipStore().addShip(ship);
+
+
+        // AverageCargoByYearScript averageCargoByYearScript = new AverageCargoByYearScript();
+        // averageCargoByYearScript.numberOfContainers(databaseConnection);
 
         return true;
     }
---
+++ src/main/java/lapr/project/data/DataBaseScripts/AverageCargoByYearScript.java
@@ -0,0 +1,109 @@
+package lapr.project.data.DataBaseScripts;
+
+import lapr.project.data.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AverageCargoByYearScript {
+
+    public AverageCargoByYearScript() {
+        //Empty constructor
+    }
+
+    private DatabaseConnection databaseConnection = null;
+    private int sumContainers = 0;
+
+    public int getCargoManifestsOfATripFromDataBase(int mmsi, int date, int j) {
+        String sqlCommand = "select cm.CARGOMANIFESTID from CARGOMANIFEST cm\n" +
+                "                                   inner join TRIP T on T.IDTRIP = cm.IDTRIP and T.VEHICLEID = cm.VEHICLEID and Extract(YEAR from CARGOMANIFESTDATE) = " + date +
+                "where t.VEHICLEID = (select v.VEHICLEID from VEHICLE v inner join Ship S on v.vehicleId = S.vehicleId\n" +
+                "                     where s.MMSI = " + mmsi + ")";
+
+        Connection connection = databaseConnection.getConnection();
+
+        try (PreparedStatement getPreparedStatement = connection.prepareStatement(sqlCommand)) {
+            try (ResultSet resultSet = getPreparedStatement.executeQuery()) {
+                for (int i = 0; i < j; i++) {
+                    resultSet.next();
+                }
+
+                if (resultSet.next()) {
+                    return resultSet.getInt("CARGOMANIFESTID");
+
+                } else return 0;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return 0;
+        }
+    }
+
+    private int getCargoManifestOfATripSize(int mmsi, int date) {
+        String sqlCommand = "select COUNT(*) COUNT_MANIFESTS from CARGOMANIFEST cm\n" +
+                "                                   inner join TRIP T on T.IDTRIP = cm.IDTRIP and T.VEHICLEID = cm.VEHICLEID and Extract(YEAR from CARGOMANIFESTDATE) = " + date +
+                "where t.VEHICLEID = (select v.VEHICLEID from VEHICLE v inner join Ship S on v.vehicleId = S.vehicleId\n" +
+                "                     where s.MMSI = " + mmsi + ")";
+
+        Connection connection = databaseConnection.getConnection();
+
+        try (PreparedStatement getPreparedStatement = connection.prepareStatement(sqlCommand)) {
+            try (ResultSet resultSet = getPreparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt("COUNT_MANIFESTS");
+
+                } else {
+                    return 0;
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return 0;
+        }
+    }
+
+    private void getContainersCargoManifestOfATripSize(int cargoManifestID) {
+        Connection connection = databaseConnection.getConnection();
+
+        String sqlCommand = "select count(c.CONTAINERID) COUNT_CONTAINERS\n" +
+                "from CONTAINER c\n" +
+                "         inner join CARGOMANIFESTCONTAINER cm on cm.CONTAINERID = c.CONTAINERID\n" +
+                "where cm.CARGOMANIFESTID = " + cargoManifestID;
+
+        try (PreparedStatement getPreparedStatement = connection.prepareStatement(sqlCommand)) {
+            try (ResultSet resultSet = getPreparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    sumContainers += resultSet.getInt("COUNT_CONTAINERS");
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public int getNumberOfContainersPerTrip(int mmsi, int date) {
+        int j = getCargoManifestOfATripSize(mmsi, date);
+        int cargoManifestID = 0;
+
+        int count = 0;
+        int aux = j;
+
+        while (j != 0) {
+            cargoManifestID = getCargoManifestsOfATripFromDataBase(mmsi, date, count);
+            getContainersCargoManifestOfATripSize(cargoManifestID);
+
+            count++;
+            j--;
+        }
+
+        return sumContainers / aux;
+    }
+
+    public int numberOfContainers(DatabaseConnection databaseConnection) {
+        this.databaseConnection = databaseConnection;
+
+        return getNumberOfContainersPerTrip(366772990, 2020);
+    }
+}
