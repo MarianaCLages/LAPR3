4082af03fcad1524a7a496a43d82e57f87bbae69
1200601 Tiago Ferreira <1200601@isep.ipp.pt>
2021-12-23 16:53:00 +0000

    LAPR22G96-250 #comment merge solving #time 1m

--- src/main/java/lapr/project/data/DataBaseScripts/CallTheAvailableResourcesFunction.java
+++ src/main/java/lapr/project/data/DataBaseScripts/CallTheAvailableResourcesFunction.java
@@ -78,10 +78,6 @@ public class CallTheAvailableResourcesFunction {
                     "Facility Area capcity:" + split2[i] + "%\n";
         }
 
-<<<<<<< HEAD
-=======
-        String resources;
->>>>>>> 1ce026a12d1305dcd733398ca4acca4a6db6ab59
 
 
 
