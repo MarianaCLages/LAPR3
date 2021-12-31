ec2a4cddf35c2427da491dac2a287cfca66217ae
1200601 Tiago Ferreira <1200601@isep.ipp.pt>
2021-12-31 01:57:17 +0000

    LAPR22G96-297 #comment some unit tests for US303 #time 20m

--- src/test/java/lapr/project/model/GraphNClosestPlacesTest.java
+++ src/test/java/lapr/project/model/GraphNClosestPlacesTest.java
@@ -6,12 +6,17 @@ import lapr.project.shared.graph.MapGraph;
 import lapr.project.shared.graph.Vertex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphNClosestPlacesTest {
 
 
     Graph<Vertex, Double> graph = new MapGraph<>(false);
+    GraphNClosestPlaces graphNClosestPlaces = new GraphNClosestPlaces();
 
     @BeforeEach
     public void setUp() throws NullVerticesException{
@@ -48,5 +53,116 @@ public class GraphNClosestPlacesTest {
                 "Origin Vertex: Bailundo , which is a : City\n";
 
         assertEquals(expected,GraphNClosestPlaces.getNClosestPlaces(graph,1));
+    }
+
+    @Test
+    void swap(){
+
+        ArrayList<LinkedList<Vertex>> arrayList = new ArrayList<>();
+        ArrayList<LinkedList<Vertex>> arrayListExpected = new ArrayList<>();
+
+        LinkedList<Vertex> v1 = new LinkedList<>();
+        LinkedList<Vertex> v2 = new LinkedList<>();
+
+        ArrayList<Double> ad = new ArrayList<>();
+        ArrayList<Double> adExpected = new ArrayList<>();
+        adExpected.add(2.2);
+        adExpected.add(1.2);
+        ad.add(1.2);
+        ad.add(2.2);
+
+        v1.add(0,new City("Madrid", 12, 36, new Country("Espanha", null, null, 7000, Continent.EUROPE)));
+        v1.add(1,new City("Bailundo", -12, 15, new Country("Angola", null, null, 8500, Continent.AFRICA)));
+        v1.add(2,new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+
+        v2.add(0, new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+        v2.add(1,new City("Berlim", 20, 45, new Country("Alemanha", null, null, 8000, Continent.EUROPE)));
+
+        arrayList.add(v1);
+        arrayList.add(v2);
+        arrayListExpected.add(v2);
+        arrayListExpected.add(v1);
+
+
+
+        graphNClosestPlaces.swap(ad,0,1,arrayList);
+
+
+        assertEquals(adExpected,ad);
+        assertEquals(arrayListExpected,arrayList);
+
+    }
+
+    @Test
+    void partitionTest(){
+
+        ArrayList<LinkedList<Vertex>> arrayList = new ArrayList<>();
+        ArrayList<LinkedList<Vertex>> arrayListExpected = new ArrayList<>();
+
+        LinkedList<Vertex> v1 = new LinkedList<>();
+        LinkedList<Vertex> v2 = new LinkedList<>();
+
+        ArrayList<Double> ad = new ArrayList<>();
+        ArrayList<Double> adExpected = new ArrayList<>();
+        adExpected.add(2.2);
+        adExpected.add(1.2);
+        ad.add(1.2);
+        ad.add(2.2);
+
+        v1.add(0,new City("Madrid", 12, 36, new Country("Espanha", null, null, 7000, Continent.EUROPE)));
+        v1.add(1,new City("Bailundo", -12, 15, new Country("Angola", null, null, 8500, Continent.AFRICA)));
+        v1.add(2,new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+
+        v2.add(0, new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+        v2.add(1,new City("Berlim", 20, 45, new Country("Alemanha", null, null, 8000, Continent.EUROPE)));
+
+        arrayList.add(v1);
+        arrayList.add(v2);
+        arrayListExpected.add(v2);
+        arrayListExpected.add(v1);
+
+
+
+        graphNClosestPlaces.partition(ad,0,1,1,arrayList);
+
+        assertEquals(arrayListExpected,arrayList);
+        assertEquals(adExpected,ad);
+
+    }
+
+    @Test
+    void quickSort(){
+
+
+        ArrayList<LinkedList<Vertex>> arrayList = new ArrayList<>();
+        ArrayList<LinkedList<Vertex>> arrayListExpected = new ArrayList<>();
+
+        LinkedList<Vertex> v1 = new LinkedList<>();
+        LinkedList<Vertex> v2 = new LinkedList<>();
+
+        ArrayList<Double> ad = new ArrayList<>();
+        ArrayList<Double> adExpected = new ArrayList<>();
+        adExpected.add(2.2);
+        adExpected.add(3.2);
+        ad.add(3.2);
+        ad.add(2.2);
+
+        v1.add(0,new City("Madrid", 12, 36, new Country("Espanha", null, null, 7000, Continent.EUROPE)));
+        v1.add(1,new City("Bailundo", -12, 15, new Country("Angola", null, null, 8500, Continent.AFRICA)));
+        v1.add(2,new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+
+        v2.add(0, new City("Roma", 28, 30, new Country("Itália", null, null, 7500, Continent.EUROPE)));
+        v2.add(1,new City("Berlim", 20, 45, new Country("Alemanha", null, null, 8000, Continent.EUROPE)));
+
+        arrayList.add(v1);
+        arrayList.add(v2);
+        arrayListExpected.add(v2);
+        arrayListExpected.add(v1);
+
+
+        graphNClosestPlaces.quickSort(ad,arrayList);
+
+        assertEquals(arrayListExpected,arrayList);
+        assertEquals(adExpected,ad);
     }
 }
