a80028abab49666685142b1168694284182104ec
1200902 Mariana Lages <1200902@isep.ipp.pt>
2022-01-19 23:30:02 +0000

    LAPR22G96-368 #comment US410 implementation fix #time 20m

--- src/main/c/SPRINT 4/energy_needed.c
+++ src/main/c/SPRINT 4/energy_needed.c
@@ -4,8 +4,10 @@ float energy_needed(Container* container_array, unsigned char x, unsigned char y
 	
 	int container_position = find_container_position(container_array, x, y, z);
 	
-	if (verifyIfRefrigerated(container_array, container_position)) {
+	if (verifyIfRefrigerated(container_array, container_position) == 1) {
 		return ((20 + 5) / container_array[container_position].thermalResistance) * VOYAGE_TIME;
+	} else if (verifyIfRefrigerated(container_array, container_position) == 0) {
+		return ((20 - 7) / container_array[container_position].thermalResistance) * VOYAGE_TIME;
 	} else {
 		return -1;
 	}
