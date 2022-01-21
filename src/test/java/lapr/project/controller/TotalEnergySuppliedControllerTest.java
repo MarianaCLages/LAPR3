4b7c22232127d7eb2b646ae83e336ef376be41a7
1200902 Mariana Lages <1200902@isep.ipp.pt>
2022-01-21 17:30:37 +0000

    LAPR22G96-380 #comment US414 tests #time 20m

---
+++ src/test/java/lapr/project/controller/TotalEnergySuppliedControllerTest.java
@@ -0,0 +1,37 @@
+package lapr.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TotalEnergySuppliedControllerTest {
+
+    TotalEnergySuppliedController controller = new TotalEnergySuppliedController();
+    int numberOfContainers = 10;
+    double temperature = 20;
+    int time = 9000;
+
+    @Test
+    void calculationToMinus5() {
+        //Arrange
+        String expected = "Journey time: 9000s\n" +
+                "Temperature: 20.0ºC\n" +
+                "Total energy to be supplied: 134338,27 J";
+        //Act
+        String actual = String.valueOf(controller.calculationToMinus5(numberOfContainers, temperature, time));
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculationTo7() {
+        //Arrange
+        String expected = "Journey time: 9000s\n" +
+                "Temperature: 20.0ºC\n" +
+                "Total energy to be supplied: 851266,50 J";
+        //Act
+        String actual = String.valueOf(controller.calculationTo7(numberOfContainers, temperature, time));
+        //Assert
+        assertEquals(expected, actual);
+    }
+}
