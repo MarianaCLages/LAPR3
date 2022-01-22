85b34dda7b871c11ae933e21ece8ffe3ba698281
1191008 Rodrigo Rodrigues <1191008@isep.ipp.pt>
2022-01-22 13:06:56 +0000

    LAPR22G96-357 #comment disabled conflicting classes

--- src/main/java/lapr/project/ui/generateNextWeekManifestsUI.java
+++ src/main/java/lapr/project/ui/generateNextWeekManifestsUI.java
@@ -1,4 +1,18 @@
 package lapr.project.ui;
 
+import lapr.project.controller.generateNextWeekManifestsController;
+import lapr.project.data.DataBaseScripts.generateNextWeekManifestsScript;
+
 public class generateNextWeekManifestsUI {
+
+    generateNextWeekManifestsController ctrl;
+
+    public generateNextWeekManifestsUI(){
+        this.ctrl = new generateNextWeekManifestsController();
+    }
+/*
+    @Override
+    public void run(){
+
+    }*/
 }
