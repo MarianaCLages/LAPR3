76c9a0335f97f6d9fafffc97959c7dc50bae520d
EduardoSousa <1200920@isep.ipp.pt>
2022-01-22 17:17:56 +0000

    LAPR22G96-417 #time 1m #comment correction on ShipStoreData

--- src/test/java/lapr/project/shared/graph/PortCentralityTest.java
+++ src/test/java/lapr/project/shared/graph/PortCentralityTest.java
@@ -5,7 +5,8 @@ import lapr.project.shared.exceptions.NullVerticesException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class PortCentralityTest {
 
