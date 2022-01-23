c634083128b5c5a9705f3d4066ffe13fbe33b291
1200601 Tiago Ferreira <1200601@isep.ipp.pt>
2022-01-23 19:20:15 +0000

    LAPR22G96-333 #comment mentioning the year

--- src/main/java/lapr/project/ui/CountDaysEachScriptUI.java
+++ src/main/java/lapr/project/ui/CountDaysEachScriptUI.java
@@ -2,6 +2,8 @@ package lapr.project.ui;
 
 import lapr.project.controller.CountDaysEachShipScriptController;
 
+import java.util.Date;
+
 public class CountDaysEachScriptUI implements Runnable{
 
     public CountDaysEachScriptUI() {
@@ -13,8 +15,9 @@ public class CountDaysEachScriptUI implements Runnable{
     public void run() {
 
         CountDaysEachShipScriptController countDaysEachShipScriptController = new CountDaysEachShipScriptController();
-
+            Date date = new Date();
             String s = countDaysEachShipScriptController.CountDaysEachShipScript(2022);
+            System.out.println("Year: 2022\n");
             System.out.println(s);
 
     }
